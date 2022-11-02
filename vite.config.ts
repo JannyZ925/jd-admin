67667eacdc907140e1a6b4dcb1a1dde05e58ab06
lexmin0412 <zhangle_dev@outlook.com>
2022-11-03 01:54:43 +0800

    feat: init project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+const PORT = 5250;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: PORT,
+  },
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
